0efa6332556404ac4c940a14d85df1438184695e
yangsuda <919579@qq.com>
2020-12-14 17:44:14 +0800

    系统参数设置修改
    增加富文本和附件上传功能

--- app/Table/SysconfigTable.php
+++ app/Table/SysconfigTable.php
@@ -7,6 +7,50 @@ use App\Core\Table;
 
 class SysconfigTable extends Table
 {
+    /**
+     * 数据获取之后的自定义处理
+     * @param $data
+     * @return array
+     */
+    public function dataViewAfter(&$data): int
+    {
+        if (!empty($data['value'])) {
+            $data['value'] = stripslashes($data['value']);
+        }
+        return 200;
+    }
+
+    /**
+     * 表单HTML获取之前的自定义处理
+     * @param $data
+     * @return array
+     */
+    public function getFormHtmlBefore(&$fields, &$data, &$form): int
+    {
+        if (defined('MANAGE') && MANAGE == 1) {
+            $value = aval($data, 'value', '');
+            $data['ueditorHtml'] = ueditor('ueditorValue', $value, ['identity' => 'admin']);
+        }
+        return 200;
+    }
+
+    /**
+     * 数据保存前的自定义处理
+     * @param $data
+     * @return array
+     */
+    public function dataSaveBefore(&$data, $row = []): int
+    {
+        if (defined('MANAGE') && MANAGE == 1) {
+            if ($data['type'] == 5) {
+                $data['value'] = self::input('ueditorValue', 'htmltext');
+            }
+            if ($data['type'] == 4) {
+                $data['value'] = self::input('value', 'addon');
+            }
+        }
+        return 200;
+    }
 
     /**
      * 数据保存后的自定义处理
