75380eb40454e79a047c64faf7df78256912d1f8
yangsuda <919579@qq.com>
2020-12-14 17:43:16 +0800

    日志记录修改

--- app/Core/settings.php
+++ app/Core/settings.php
@@ -3,12 +3,9 @@ declare(strict_types=1);
 
 use function DI\autowire;
 use DI\ContainerBuilder;
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
-use Monolog\Processor\UidProcessor;
-use Monolog\Formatter\LineFormatter;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
+use SlimCMS\Helper\File;
 use SlimCMS\Interfaces\RouteInterface;
 use SlimCMS\Interfaces\CookieInterface;
 use SlimCMS\Interfaces\OutputInterface;
@@ -21,7 +18,6 @@ use SlimCMS\Core\Cookie;
 use SlimCMS\Core\Output;
 use SlimCMS\Core\Database;
 
-
 return function (ContainerBuilder $containerBuilder) {
     //Session保存路径
     $sessSavePath = CSDATA . "/sessions/";
@@ -29,8 +25,10 @@ return function (ContainerBuilder $containerBuilder) {
         session_save_path($sessSavePath);
     }
 
-    //Session跨域设置,为方便调试，本地注释掉
-    //@session_set_cookie_params(0,'/','.'.$this->cfg['cfg']['domain']);
+    //Session跨域设置,为方便调试，debug开启不设置
+    if (CORE_DEBUG === false) {
+        @session_set_cookie_params(0, '/', '.' . $this->cfg['cfg']['domain']);
+    }
 
     //时区设置
     @date_default_timezone_set('Etc/GMT-8');
@@ -40,26 +38,7 @@ return function (ContainerBuilder $containerBuilder) {
 
     $containerBuilder->addDefinitions([
         LoggerInterface::class => DI\factory(function (ContainerInterface $c) {
-            $settings = $c->get('settings');
-            $fileName = substr(md5($settings['security']['authkey']), 5, -10);
-            //日期格式
-            $dateFormat = "Y-m-d H:i:s";
-            //输出格式
-            $output = "[%datetime%] - [%level_name%] - [%channel%]\n%message% %context% %extra%\n";
-            //创建一个格式化器
-            $formatter = new LineFormatter($output, $dateFormat);
-
-            $path = CSDATA . 'logs_'.$fileName.'/' . date('Y-m-d') . '.log';
-            $logger = new Logger('slimCMS');
-
-            $processor = new UidProcessor();
-            $logger->pushProcessor($processor);
-
-            $handler = new StreamHandler($path, 100);
-            $handler->setFormatter($formatter);
-            $logger->pushHandler($handler);
-
-            return $logger;
+            return File::log();
         }),
         RouteInterface::class => autowire(Routes::class),
         CookieInterface::class => function (ContainerInterface $c) {
