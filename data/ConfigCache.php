4efa8b203d42487ebb2bd2f2f63126ea13a5ed5d
yangsuda <919579@qq.com>
2020-12-21 14:07:04 +0800

    缓存配置文件

---
+++ data/ConfigCache.php
@@ -0,0 +1,63 @@
+<?php
+return array (
+  'cfg' => 
+  array (
+    'waterpos' => '9',
+    'markimg' => '',
+    'transport' => 'http',
+    'public_path' => '',
+    'private_path' => '',
+    'alicacert' => '',
+    'input_charset' => 'utf-8',
+    'sign_type' => 'RSA',
+    'seller_email' => '',
+    'alipaykey' => '',
+    'partner' => '',
+    'rootca' => '',
+    'sslkey_path' => '',
+    'sslcert_path' => '',
+    'wxkey' => '',
+    'mchid' => '',
+    'appsecret' => '',
+    'appid' => '',
+    'tokenCheck' => '1',
+    'refererWhite' => '',
+    'attachmentHost' => '',
+    'entryFileName' => 'index.php',
+    'jpgQuality' => '95',
+    'picHeight' => '800',
+    'picWidth' => '800',
+    'whitePicUrl' => '/uploads/
+http://wx.qlogo.cn
+https://wx.qlogo.cn
+http://thirdwx.qlogo.cn
+https://thirdwx.qlogo.cn
+http://q.qlogo.cn
+https://q.qlogo.cn
+http://thirdqq.qlogo.cn
+https://thirdqq.qlogo.cn',
+    'tokenTTL' => '86400',
+    'waterMark' => '0',
+    'rewriteUrl' => '0',
+    'urlEncrypt' => '0',
+    'webname' => 'slimCMS',
+    'imgHeight' => '5000',
+    'imgWidth' => '5000',
+    'imgtype' => 'jpg|gif|png|jpeg',
+    'softtype' => 'zip|gz|rar|iso|doc|xsl|ppt|wps|apk|xls|txt|mp3|apk|xml|xlsx|pem',
+    'mediatype' => 'swf|mpg|mp3|rm|rmvb|wmv|wma|wav|mid|mov|flv|swf|mp4',
+    'notallowstr' => '迷药|魂药|裸体|麻黄|羟亚|裸聊|赌博|避孕|猎枪|仿真枪|手枪|枪支|卖枪|买枪|毒品|麻将|催情|催眠|符咒|伟哥|六合彩|轮盘|情粉|迷昏|定情药|迷情药|定位仪|追踪仪|赌场|地磅遥控|地磅调节器|按摸|居住证|暂住证|毕业证|西药|中药|魂药|乙醚|汽枪|监听器|追踪器|干扰器|迷幻药|拦截器|定位器|窃听|手机跟踪|手机定位|成人用品|文凭|羟亚胺|麻黄素|盐酸麻黄碱|氯胺酮|冰毒|盐酸羟亚胺|黄冰糖|罂粟壳|麻古|甲基苯丙胺|摇头丸|网络兼职|在家办公|代扣分|代考|收发票|套现|刷信誉|代写|高仿|娱乐城|博彩|弓箭|弓弩|监听|警用|电棍|定额发票|工资日结|爆炸|操&#042;&#042;&#042;|法轮功|反共|共党|军用|找小姐|枪支|办证|毛泽东|江泽民|邓小平|习近平|李克强|胡锦涛|王岐山|张高丽|朱镕基|解码器|老虎机|海洛因|冰毒|薄熙来|温家宝|王立军|周永康|政变|李洪志|找MM|上门服|小姐服务|美女兼职|学生妹',
+    'adminLog' => '1',
+    'maxUploadSize' => '10240',
+    'replacestr' => '她妈|它妈|他妈|你妈|去死|贱人',
+    'ccode' => '0',
+    'isDelAttachment' => '1',
+    'keywords' => '',
+    'description' => '',
+    'imgFull' => '0',
+    'imgBgcolor' => '0',
+    'domain' => '',
+    'resourceUrl' => '',
+    'basehost' => '',
+  ),
+);
