c228e7c81d6a6553cf840b054c5c06e32c2827db
919579 <919579@qq.com>
2022-08-30 10:20:37 +0800

    图集上传删除操作改成后台接口，防止前端删除风险

--- app/Control/admincp/ImageControl.php
+++ app/Control/admincp/ImageControl.php
@@ -1,24 +1,23 @@
 <?php
 
 /**
- * 图片处理控制类,防止前端直接删除图片，默认相应接口不开放
+ * 图片处理控制类
  * @author zhucy
  */
 
 declare(strict_types=1);
 
-namespace App\Control\main;
+namespace App\Control\admincp;
 
 use App\Core\Forms;
-use SlimCMS\Abstracts\ControlAbstract;
 use SlimCMS\Interfaces\UploadInterface;
 
-class ImageControl extends ControlAbstract
+class ImageControl extends AdmincpControl
 {
     /**
      * 图片上传组件传图
      */
-    /*public function webupload()
+    public function webupload()
     {
         $post = [];
         $post['files'] = aval($_FILES, 'file');
@@ -32,12 +31,12 @@ class ImageControl extends ControlAbstract
             echo $res->getData()['fileid'];
         }
         exit;
-    }*/
+    }
 
     /**
      * 删除传图组件指定图片
      */
-    /*public function webuploadDel()
+    public function webuploadDel()
     {
         isset($_SESSION) ? '' : session_start();
         $id = self::input('id');
@@ -49,12 +48,12 @@ class ImageControl extends ControlAbstract
         unset($_SESSION['file_info'][$id]);
         unset($_SESSION['bigfile_info'][$id]);
         exit("已删除");
-    }*/
+    }
 
     /**
      * 传图组件缩略图显示
      */
-    /*public function webuploadThumbnail()
+    public function webuploadThumbnail()
     {
         isset($_SESSION) ? '' : session_start();
         $id = self::input('id');
@@ -67,13 +66,13 @@ class ImageControl extends ControlAbstract
         header('Content-type: image/jpeg');
         header('Content-Length: ' . strlen($_SESSION['file_info'][$id]));
         exit($_SESSION['file_info'][$id]);
-    }*/
+    }
 
     /**
      * 删除图集中的某张图片
      * @throws \SlimCMS\Error\TextException
      */
-    /*public function webuploadImageDel()
+    public function webuploadImageDel()
     {
         $fid = self::inputInt('fid');
         $id = self::inputInt('id');
@@ -81,5 +80,5 @@ class ImageControl extends ControlAbstract
         $pic = self::inputString('pic');
         $res = Forms::imgsDel($fid, $id, $field, $pic);
         return self::response($res);
-    }*/
+    }
 }
