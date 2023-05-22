708a0825ebadbcf690279e0c9d166074f7ad3d79
yangsuda <919579@qq.com>
2023-05-22 08:43:42 +0800

    apiUpdate方法调用判断修改

--- app/Control/admincp/FormsControl.php
+++ app/Control/admincp/FormsControl.php
@@ -77,8 +77,8 @@ class FormsControl extends AdmincpControl
             $referer = self::input('referer', 'url');
             $referer = $referer ?: self::url('&p=forms/dataList&id=');
             $res = Forms::dataSave($fid, $id, [], ['admin' => self::$admin])->withReferer($referer);
-            //插件勾子调用
-            PluginModel::hook('api', 'apiUpdate', ['formid' => $fid, 'fieldid' => $id]);
+            //插件勾子调用,用于更新接口文档
+            $fid == 2 && PluginModel::hook('api', 'apiUpdate', ['formid' => $fid, 'fieldid' => $id]);
             return $this->directTo($res);
         }
         $options = ['cacheTime' => 300, 'ueditorType' => 'admin', 'admin' => self::$admin];
