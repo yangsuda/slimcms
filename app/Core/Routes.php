43739208d6b796bc6d8b5a10bcd38c3a23eb6e9b
yangsuda <919579@qq.com>
2025-07-07 13:54:27 +0800

    提交方式判断修改

--- app/Core/Routes.php
+++ app/Core/Routes.php
@@ -3,7 +3,17 @@ declare(strict_types=1);
 
 namespace App\Core;
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+
 class Routes extends \SlimCMS\Core\Routes
 {
-
+    public function route(App $app)
+    {
+        $app->options('/[{params:.*}]', function (ServerRequestInterface $req, ResponseInterface $res) {
+            return $res;
+        });
+        $app->map(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], '/[{params:.*}]', $this->routeCallable($app));
+    }
 }
