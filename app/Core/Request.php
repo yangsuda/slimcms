2b5626a088aea0360af1ccb8929735d2cc48f64f
yangsuda <919579@qq.com>
2026-01-26 14:31:54 +0800

    校验方式改造

--- app/Core/Request.php
+++ app/Core/Request.php
@@ -7,28 +7,6 @@ declare(strict_types=1);
 
 namespace App\Core;
 
-use Respect\Validation\Exceptions\ValidationException;
-use SlimCMS\Error\TextException;
-
 class Request extends \SlimCMS\Core\Request
 {
-    protected function inputData(array $param): array
-    {
-        $data = [];
-        foreach ($param as $k => $v) {
-            $val = $this->getInput($k);
-            if ($v instanceof \Respect\Validation\Validatable) {
-                try {
-                    $v->assert($val);
-                } catch (ValidationException $e) {
-                    !empty($e->getParams()['template']) && $e->updateTemplate($e->getParams()['template']);
-                    throw new TextException(21000, ['msg' => $e->getMessage()]);
-                }
-                isset($val) && $data[$k] = $val;
-                unset($param[$k]);
-            }
-        }
-        $data = array_merge($data, parent::inputData($param));
-        return $data;
-    }
 }
